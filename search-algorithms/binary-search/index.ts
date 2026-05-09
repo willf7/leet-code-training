ae63fa13f22a71ebdec7171e1ef0df9f75daf84a
William Felipe <williamfelipe081@gmail.com>
2026-05-08 22:43:21 -0300

    feat: add binary search algorithm

---
+++ search-algorithms/binary-search/index.ts
@@ -0,0 +1,26 @@
+function binarySearch(nums: Array<number>, target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let middle = Math.floor((left + right) / 2);
+
+        if (nums[middle] == target) {
+            return middle;
+        }
+
+        if (nums[middle] > target) {
+            right = middle - 1;
+        }
+
+        if (nums[middle] < target) {
+            left = middle + 1;
+        }
+    }
+
+    return -1
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8], 6));
+console.log(binarySearch([3,5,6,7,9,16,17,28], 28));
+console.log(binarySearch([3,5,6,7,9,16,17,28], 4));
