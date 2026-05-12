ab12d02059f099a66071fd2d52f6a7dd8bca50c9
William Felipe <williamfelipe081@gmail.com>
2026-05-11 23:18:54 -0300

    feat: add second binary search implementation with console tests

--- search-algorithms/binary/index.ts
+++ search-algorithms/binary/index.ts
@@ -21,8 +21,34 @@ function binarySearch(nums: Array<number>, target: number): number {
     return -1
 }
 
+function binarySearch2(nums: Array<number>, target: number): number {
+    let left = 0;
+    let right = nums.length;
+
+    do {
+        let middle = Math.floor(left + (right - left) / 2);
+        let value = nums[middle];
+
+        if (value === target) {
+            return middle;
+        } else if (value > target) {
+            right = middle;
+        } else {
+            left = middle + 1;
+        }
+    } while (left < right);
+
+    return -1
+}
+
+console.log('With first implementation: ')
 console.log(binarySearch([1,2,3,4,5,6,7,8], 6));
 console.log(binarySearch([3,5,6,7,9,16,17,28], 28));
 console.log(binarySearch([3,5,6,7,9,16,17,28], 4));
+
+console.log('With second implementation: ')
+console.log(binarySearch2([1,2,3,4,5,6,7,8], 6));
+console.log(binarySearch2([3,5,6,7,9,16,17,28], 28));
+console.log(binarySearch2([3,5,6,7,9,16,17,28], 4));
 
 // O(log n)
