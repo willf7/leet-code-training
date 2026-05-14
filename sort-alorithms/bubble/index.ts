0fa8c1c6244281c7e4d5ea74fbf7f21ae01c134f
William Felipe <williamfelipe081@gmail.com>
2026-05-14 20:51:44 -0300

    feat: add second implementation of bubble sort and update test cases

--- sort-alorithms/bubble/index.ts
+++ sort-alorithms/bubble/index.ts
@@ -23,8 +23,22 @@ function bubbleSort(nums: number[]): number[] {
     return nums;
 };
 
+function bubbleSort2(nums: number[]): number[] {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 0; j < nums.length -1 - i; j++) {
+            if (nums[j] > nums[j + 1]) {
+                let tmp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j  + 1] = tmp;
+            }
+        }
+    }
+
+    return nums;
+}
+
 console.log(bubbleSort([9,4,7,1,3])); // Expected output: [1,3,4,7,9]
-console.log(bubbleSort([10,8,6,4,2])); // Expected output: [2,4,6,8,10]
+console.log(bubbleSort2([10,8,6,4,2])); // Expected output: [2,4,6,8,10]
 
 // O(n^2)
 
